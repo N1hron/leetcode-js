51403f16aa45dac343ff8fffb33d1ea4ddff5014
Denis <n1hron@yandex.com>
2025-12-01 15:46:35 +0300

    add globals to eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,17 @@
 import js from '@eslint/js';
 import stylistic from '@stylistic/eslint-plugin';
+import globals from "globals";
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ['**/*.js'], plugins: { js }, extends: ['js/recommended'] },
+  { 
+    files: ['**/*.js'], 
+    plugins: { js }, 
+    extends: ['js/recommended'], 
+    languageOptions: { 
+      globals: globals.node 
+    } 
+  },
   {
     plugins: {
       '@stylistic': stylistic,
