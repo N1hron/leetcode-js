aa4888edf85ceb882b1fb9f32bd6d5310d016a30
Denis <n1hron@yandex.com>
2025-12-01 15:20:23 +0300

    2620. Counter

---
+++ counter/index.js
@@ -0,0 +1,7 @@
+export const createCounter = function(n) {
+  let count = n;
+  
+  return function() {
+    return count++;
+  };
+};
---
+++ counter/index.test.js
@@ -0,0 +1,21 @@
+import { describe, expect, it } from 'vitest';
+
+import { createCounter } from '.';
+
+describe('createCounter', () => {
+  it('creates counter function that returns 1 more than the previous value', () => {
+    let counter = createCounter(10);
+
+    expect(counter()).toBe(10);
+    expect(counter()).toBe(11);
+    expect(counter()).toBe(12);
+
+    counter = createCounter(-2);
+
+    expect(counter()).toBe(-2);
+    expect(counter()).toBe(-1);
+    expect(counter()).toBe(0);
+    expect(counter()).toBe(1);
+    expect(counter()).toBe(2);
+  });
+});
