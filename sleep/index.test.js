45e32a8abfc95bcd68de2378230222d0b3baa62e
Denis <n1hron@yandex.com>
2025-12-01 15:47:02 +0300

    2621. Sleep

---
+++ sleep/index.test.js
@@ -0,0 +1,22 @@
+import { describe, expect, it, vi } from 'vitest';
+
+import { sleep } from '.';
+
+describe('sleep', () => {
+  it('should return a promise that resolves after specified time', async () => {
+    vi.useFakeTimers();
+
+    let cb = vi.fn();
+
+    const millis = 1000;
+    sleep(millis).then(cb);
+
+    expect(cb).not.toHaveBeenCalled();
+    await vi.advanceTimersByTimeAsync(millis - 1);
+    expect(cb).not.toHaveBeenCalled();
+    await vi.advanceTimersByTimeAsync(1);
+    expect(cb).toHaveBeenCalledOnce();
+
+    vi.useRealTimers();
+  });
+});
