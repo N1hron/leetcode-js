587c613d0930d3c0aec4ba782d190793c69592cf
Denis <n1hron@yandex.com>
2025-12-02 17:54:26 +0300

    format eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -4,13 +4,13 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { 
-    files: ["**/*.js"], 
-    plugins: { js }, 
-    extends: ["js/recommended"], 
-    languageOptions: { 
-      globals: globals.node 
-    } 
+  {
+    files: ["**/*.js"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.node,
+    },
   },
   {
     plugins: {
@@ -20,6 +20,25 @@ export default defineConfig([
       "@stylistic/semi": "error",
       "@stylistic/indent": ["error", 2],
       "@stylistic/quotes": ["error", "double"],
+      "@stylistic/comma-dangle": ["error", "always-multiline"],
+      "@stylistic/comma-spacing": "error",
+      "@stylistic/no-multi-spaces": "error",
+      "@stylistic/arrow-parens": ["error", "always"],
+      "@stylistic/block-spacing": "error",
+      "@stylistic/brace-style": ["error", "1tbs"],
+      "@stylistic/object-curly-spacing": ["error", "always"],
+      "@stylistic/padded-blocks": ["error", "never"],
+      "@stylistic/no-multiple-empty-lines": ["error", { max: 1 }],
+      "@stylistic/key-spacing": "error",
+      "@stylistic/jsx-tag-spacing": "error",
+      "@stylistic/no-trailing-spaces": "error",
+      "@stylistic/space-infix-ops": "error",
+      "@stylistic/member-delimiter-style": ["error", {
+        multiline: {
+          delimiter: "semi",
+          requireLast: true,
+        },
+      }],
     },
   },
 ]);
